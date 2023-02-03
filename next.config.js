f7a21041a6fcf43bc5728cc0eb5244b4b411340d
aarish <aarishkhan13111@gmail.com>
2023-02-03 18:36:20 +0530

    add env files

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    MONGO_URI:
-      "mongodb+srv://arish13111:Aarish13111@cluster0.c6dtpxr.mongodb.net/?retryWrites=true&w=majority",
-  },
 };
 
 module.exports = nextConfig;
